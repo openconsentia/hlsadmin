1b56888cbd42b9ac3c01dee5107248d64445c3ba
Paul Sitoh <paulwizviz@gmail.com>
2020-04-13 21:16:04 +0100

    Get webpack to optimize code
    This is for production artefacts.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- web/reactjs/webpack/webpack.base.js
+++ web/reactjs/webpack/webpack.base.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const BUILD_DIR = path.resolve(__dirname, '..', 'public');
 const SRC_DIR = path.resolve(__dirname, '..', 'src');
+const BUILD_DIR = path.resolve(__dirname, '..', 'public');
 
 module.exports = {
   entry: [`@babel/polyfill`,`${SRC_DIR}/index.js`],
